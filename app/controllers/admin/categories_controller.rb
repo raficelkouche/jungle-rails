94cc0a684bea1856c54e91deb853f695174bc739
Rafic El Kouche <raficelkouche@hotmail.com>
2021-02-05 01:45:17 +0000

    add feature for admins to manage categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,20 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    puts @categories.inspect
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(params.require(:category).permit(:name))
+ 
+    if @category.save
+      redirect_to admin_categories_path
+    else
+      render 'new'
+    end
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
-
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  
   has_many :products
 
 end
